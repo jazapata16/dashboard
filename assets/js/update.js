e540442fb273be469cdb93c450380608cce9ca1a
jazapata16 <107829063+jazapata16@users.noreply.github.com>
2023-03-13 07:06:26 -0500

    S02D01

--- assets/js/update.js
+++ assets/js/update.js
@@ -1,4 +1,27 @@
 let runScripts=()=>{
     let chart_bars = document.getElementById("chart-bars");
     console.log(chart_bars);
+    let icons = document.getElementsByTagName("i");
+    for (let icon of icons){
+        console.log(icons);
+    }
+    let cards= document.getElementsByClassName("card");
+    for (let card of cards){
+        console.log(card);
+    }
 }
+
+runScripts();
+
+let modifyText=()=>{
+
+    let listOfP = document.getElementsByTagName("p");
+    let elementP= listOfP[3]
+    elementP.innerHTML="Dinero Actual"
+
+    let listOfH4 = document.getElementsByTagName("h4");
+    let elementH4= listOfP[0]
+    elementH4.innerHTML="301k"
+
+}
+modifyText();
